bfcce31865510abb67f4d1e767bc2bb44549594f
Balaji V <kuttibalaji.v6@gmail.com>
2021-01-28 11:56:09 +0530

    Improve UI render performance for preference widget
    Signed-off-by: Balaji V <kuttibalaji.v6@gmail.com>

--- packages/preferences/src/browser/views/preference-editor-widget.tsx
+++ packages/preferences/src/browser/views/preference-editor-widget.tsx
@@ -17,6 +17,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { postConstruct, injectable, inject } from 'inversify';
 import * as React from 'react';
+import { AutoSizer, ScrollParams } from 'react-virtualized';
 import { Disposable } from 'vscode-jsonrpc';
 import {
     ReactWidget,
@@ -26,8 +27,11 @@ import {
     CompositeTreeNode,
     SelectableTreeNode,
     PreferenceItem,
+    TreeWidget,
+    TreeNode,
+    Iterators,
 } from '@theia/core/lib/browser';
-import { Message, } from '@theia/core/lib/browser/widgets/widget';
+import { Message } from '@theia/core/lib/browser/widgets/widget';
 import { SinglePreferenceDisplayFactory } from './components/single-preference-display-factory';
 import { Preference } from '../util/preference-types';
 import { PreferencesEventService } from '../util/preference-event-service';
@@ -46,6 +50,7 @@ export class PreferencesEditorWidget extends ReactWidget {
     protected scrollContainerRef: React.RefObject<HTMLDivElement> = React.createRef();
     protected hasRendered = false;
     protected _preferenceScope: Preference.SelectedScopeDetails = Preference.DEFAULT_SCOPE;
+    protected rows = new Map<string, TreeWidget.NodeRow>();
 
     @inject(PreferencesEventService) protected readonly preferencesEventService: PreferencesEventService;
     @inject(PreferenceService) protected readonly preferenceValueRetrievalService: PreferenceService;
@@ -64,6 +69,12 @@ export class PreferencesEditorWidget extends ReactWidget {
         this.preferencesEventService.onNavTreeSelection.event(e => this.scrollToEditorElement(e.nodeID));
         this.currentDisplay = this.preferenceTreeProvider.currentTree;
         this.properties = this.preferenceTreeProvider.propertyList;
+        this.addEventListener<any>(this.node, 'ps-scroll-y', (e: Event & { target: { scrollTop: number } }) => {
+            if (this.treeRef?.list && this.treeRef.list.Grid) {
+                const { scrollTop } = e.target;
+                this.treeRef.list.Grid.handleScrollEvent({ scrollTop });
+            }
+        });
         this.update();
     }
 
@@ -87,15 +98,76 @@ export class PreferencesEditorWidget extends ReactWidget {
         });
     }
 
-    protected render(): React.ReactNode {
-        const visibleCategories = this.currentDisplay.children.filter(category => category.visible);
+    protected treeRef: TreeWidget.View | null;
+
+    protected render = (): React.ReactNode => {
+        this.doUpdateRows();
+        const rows = Array.from(this.rows.values());
         return (
             <div className="settings-main">
                 <div ref={this.scrollContainerRef} className="settings-main-scroll-container" id="settings-main-scroll-container">
-                    {!!visibleCategories.length ? visibleCategories.map(category => this.renderCategory(category as Preference.Branch)) : this.renderNoResultMessage()}
+                    {rows.length > 0 ?
+                        <AutoSizer>
+                            {({ height, width }) => (
+                                <TreeWidget.View ref={ref => this.treeRef = ref} height={height} width={width} rows={rows}
+                                    handleScroll={this.handleScroll} renderNodeRow={this.renderRow} />)}
+                        </AutoSizer> : this.renderNoResultMessage()}
                 </div>
             </div>
         );
+    };
+
+    protected renderRow = (row: TreeWidget.NodeRow): React.ReactNode => {
+        const category = row.node;
+
+        const isCategory = category.parent?.parent === undefined;
+        const categoryLevelClass = isCategory ? 'settings-section-category-title' : 'settings-section-subcategory-title';
+        if (Preference.Branch.is(category)) {
+            return (
+                <div className={categoryLevelClass} data-id={category.id} key={`${category.id}-editor`}
+                    id={`${category.id}-editor`}>{category.name}</div>
+            );
+        } else if (SelectableTreeNode.is(category)) {
+            const preferenceNode = category;
+            const values = this.preferenceValueRetrievalService.inspect<PreferenceItem>(preferenceNode.id, this.activeURI);
+            const preferenceNodeWithValueInAllScopes = { ...preferenceNode, preference: { data: this.properties[preferenceNode.id], values } };
+            return this.singlePreferenceFactory.render(preferenceNodeWithValueInAllScopes);
+        }
+    };
+
+    protected handleScroll = (scrollParams: ScrollParams): void => {
+        this.node.scrollTop = scrollParams.scrollTop;
+    };
+
+    protected doUpdateRows(): void {
+        this.treeRef?.cache.clearAll();
+        const rowsToUpdate: Array<[string, TreeWidget.NodeRow]> = [];
+        if (this.currentDisplay) {
+            const depths = new Map<CompositeTreeNode | undefined, number>();
+            let index = 0;
+            const iterator = Iterators.depthFirst(this.currentDisplay, (node: CompositeTreeNode) => {
+                if (Preference.Branch.is(node)) {
+                    return node.children.concat(node.leaves).sort((a, b) => this.sort(a.id, b.id));
+                } else if (node.children) {
+                    return node.children.map(child => child);
+                }
+            }, node => !!node.visible);
+            for (const node of iterator) {
+                if (TreeNode.isVisible(node)) {
+                    const parentDepth = depths.get(node.parent);
+                    const depth = parentDepth === undefined ? 0 : TreeNode.isVisible(node.parent) ? parentDepth + 1 : parentDepth;
+                    if (CompositeTreeNode.is(node)) {
+                        depths.set(node, depth);
+                    }
+                    rowsToUpdate.push([node.id, {
+                        index: index++,
+                        node,
+                        depth
+                    }]);
+                }
+            }
+        }
+        this.rows = new Map(rowsToUpdate);
     }
 
     protected handleChangeDisplay = (didGenerateNewTree: boolean): void => {
@@ -120,14 +192,17 @@ export class PreferencesEditorWidget extends ReactWidget {
         }
     };
 
-    protected addFirstVisibleChildId(container: Element, array: string[]): void {
-        const children = container.children;
-        for (let i = 0; i < children.length && !array.length; i++) {
-            const id = children[i].getAttribute('data-id');
-            if (id && this.isInView(children[i] as HTMLElement, container as HTMLElement)) {
-                array.push(id);
-            } else if (!array.length) {
-                this.addFirstVisibleChildId(children[i], array);
+    protected addFirstVisibleChildId(container: HTMLElement, array: string[]): void {
+        const grid = container.getElementsByClassName('ReactVirtualized__Grid__innerScrollContainer')[0];
+        if (grid) {
+            for (let i = 0; i < grid.children.length && !array.length; i++) {
+                const listElement = grid.children[i];
+                if (this.isInView(listElement as HTMLElement, this.node)) {
+                    const id = listElement.children[0]?.getAttribute('data-id');
+                    if (id) {
+                        array.push(id);
+                    }
+                }
             }
         }
     }
@@ -183,8 +258,18 @@ export class PreferencesEditorWidget extends ReactWidget {
         if (nodeID) {
             const el = document.getElementById(`${nodeID}-editor`);
             if (el) {
-                // Timeout to allow render cycle to finish.
                 setTimeout(() => el.scrollIntoView());
+            } else {
+                const treeNode = this.rows.get(nodeID);
+                if (treeNode) {
+                    setTimeout(() => {
+                        this.treeRef?.list?.scrollToRow(treeNode.index);
+                        const elAfterScroll = document.getElementById(`${nodeID}-editor`);
+                        if (elAfterScroll) {
+                            elAfterScroll.scrollIntoView();
+                        }
+                    });
+                }
             }
         }
     }
